a544e591b0147aeb1075911f50cb2f2495371fc5
Leonel <leonelpiedrabuena04@gmail.com>
2024-05-29 23:57:48 -0300

    GET pacientes
    Realizados los mismos pasos para pacientes. Posible error en campo 'urbanización'.

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,14 +1,15 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.paciente.DatosListaPaciente;
 import med.voll.api.paciente.DatosRegistroPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -20,5 +21,10 @@ public class PacienteController {
     @PostMapping
     public void registrar(@RequestBody @Valid DatosRegistroPaciente datos) {
         pacienteRepository.save(new Paciente(datos));
+    }
+
+    @GetMapping
+    public Page<DatosListaPaciente> listar(@PageableDefault(page = 0, size = 10, sort = {"nombre"}) Pageable paginacion) {
+        return pacienteRepository.findAll(paginacion).map(DatosListaPaciente::new);
     }
 }
