f3e125741d991109da67f0e8591241482bd8b6d4
Leonel <leonelpiedrabuena04@gmail.com>
2024-06-18 14:43:47 -0300

    Documentación Spring con Swagger

--- api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -28,6 +28,7 @@ public class SecurityConfigurations {
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers("/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated()
                         .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class))
                 .build();
